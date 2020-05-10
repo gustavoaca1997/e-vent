802db7c48d7b69d7411b2de62d0441a3a050ea31
Gustavo Castellanos <gustavoaca1997@gmail.com>
2020-05-10 09:00:05 -0400

    Receive Bluetooth Server's parameters thorugh serial communication

--- e-vent.ino
+++ e-vent.ino
@@ -161,12 +161,12 @@ void setup() {
 //////////////////
 
 void loop() {
-  if (DEBUG) {
-    if (Serial.available() > 0) {
-      setState((States) Serial.parseInt());
-      while(Serial.available() > 0) Serial.read();
-    }
-  }
+  // if (DEBUG) {
+  //   if (Serial.available() > 0) {
+  //     setState((States) Serial.parseInt());
+  //     while(Serial.available() > 0) Serial.read();
+  //   }
+  // }
 
   // All States
   tLoopTimer = now();  // Start the loop timer
@@ -307,9 +307,9 @@ void loop() {
 /////////////////
 
 void Knobs::begin() {
-  volume_.begin(&readVolume);
-  bpm_.begin(&readBpm);
-  ie_.begin(&readIeRatio);
+  volume_.begin(&readBluetoothVolume);
+  bpm_.begin(&readBluetoothBPM);
+  ie_.begin(&readBluetoothIE);
   ac_.begin(&readAc);
 }
 
@@ -381,4 +381,87 @@ void setupLogger() {
   // logger.addVar("HighPresAlarm", &alarm.getHighPressure());
   // begin called after all variables added to include them all in the header
   logger.begin(&Serial, SD_SELECT);
+}
+
+/////////////////////////
+// Bluetooth Utilities //
+////////////////////////
+
+float RR = 0.0,   // Respiratory Rate (RR) (breaths per minute): between 8 – 40.
+      TV = 0.0,   // Tidal Volume (TV) (air volume pushed into lung): between 200 – 800 mL based on patient weight.
+      IE = 0.0;   // I/E Ratio (inspiratory/expiration time ratio): recommended to start around 1:2; best if adjustable between range of 1:1 – 1:4.
+
+const char endCharacter = ':';
+
+enum BlueState {
+  OFF_BLUE_STATE,     //  0
+  ON_BLUE_STATE,      //  1
+  VOLUME_BLUE_STATE,  //  2
+  BPM_BLUE_STATE,     //  3
+  IE_BLUE_STATE       //  4
+};
+
+BlueState blueState = OFF_BLUE_STATE;
+
+BlueState charToBlueState(char c) {
+  switch (c)
+  {
+  case 'V':
+    return VOLUME_BLUE_STATE;
+
+  case 'R':
+    return BPM_BLUE_STATE;
+
+  case 'I':
+    return IE_BLUE_STATE;
+  
+  case '0':
+    return OFF_BLUE_STATE;
+    
+  default:
+    return blueState;
+  }
+}
+
+float readBluetoothValue(BlueState valueState, float* value, float (*read_fun)()) {
+  while (Serial.available() > 0 && Serial.peek() == endCharacter) {
+    Serial.read();
+  }
+  
+  if (Serial.available() > 0) {
+    if (blueState == OFF_BLUE_STATE) {
+      blueState = ON_BLUE_STATE;
+    }
+    
+    if (blueState == ON_BLUE_STATE) {
+      blueState = charToBlueState(Serial.read());
+      Serial.print("New state: ");
+      Serial.println(blueState);
+    }
+  }
+
+  if (Serial.available() > 0 && blueState == valueState) {
+    *value = Serial.parseFloat();
+    Serial.print('\t');
+    Serial.println(*value);
+    blueState = ON_BLUE_STATE;
+    return *value;
+  }
+
+  if (blueState == OFF_BLUE_STATE) {
+    return read_fun();
+  }
+  return *value;
+}
+
+float readBluetoothVolume() {
+  return readBluetoothValue(VOLUME_BLUE_STATE, &TV, &readVolume);
+}
+
+float readBluetoothBPM() {
+  return readBluetoothValue(BPM_BLUE_STATE, &RR, &readBpm);
+}
+
+float readBluetoothIE() {
+  return readBluetoothValue(IE_BLUE_STATE, &IE, &readIeRatio);
 }
